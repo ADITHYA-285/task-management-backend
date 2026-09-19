25c3740d220bbc6dd73c73d52b070cfda10b52af
Mohan G <symbolequal@gmail.com>
2026-09-19 23:51:27 +0530

    Fix Prisma MariaDB adapter configuration

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -3,7 +3,16 @@ require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
 
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+const url = new URL(process.env.DATABASE_URL);
+
+const adapter = new PrismaMariaDb({
+    host: url.hostname,
+    port: Number(url.port),
+    user: decodeURIComponent(url.username),
+    password: decodeURIComponent(url.password),
+    database: url.pathname.substring(1),
+    connectionLimit: 5,
+});
 
 const prisma = new PrismaClient({
     adapter,
