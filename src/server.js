95ba2246da208417fe4ba4c8b5fd8e87511a43fe
A S Adithya <adithyaas2217@gmail.com>
2026-09-20 11:55:44 +0530

    Fix CORS configuration

--- src/server.js
+++ src/server.js
@@ -10,61 +10,18 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://task-management-frontend-313q77smc-adithyas-projects-e20db2dd.vercel.app"
+        "https://task-management-frontend-88qmkvf87-adithyas-projects-e20db2dd.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://task-management-frontend-313q77smc-adithyas-projects-e20db2dd.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
-
-app.options("*", cors());
 app.use(express.json());
+
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const authMiddleware = require("./middleware/authmiddleware");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
-
-app.get("/", async (req, res) => {
-    try {
-        const userCount = await prisma.user.count();
-
-        res.json({
-            message: "Task Management API is running",
-            database: "Connected",
-            users: userCount
-        });
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            message: "Database connection failed"
-        });
-    }
-});
-app.get("/api/protected", authMiddleware, (req, res) => {
-    res.json({
-        message: "You accessed a protected route",
-        user: req.user
-    });
-});
-const PORT = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
-
-module.exports = app;
