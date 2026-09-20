f1505fe434df0fc52fcca6a6bfe801367a3371ae
A S Adithya <adithyaas2217@gmail.com>
2026-09-20 11:37:27 +0530

    added the cors information in the backend

--- src/server.js
+++ src/server.js
@@ -7,7 +7,27 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://task-management-frontend-313q77smc-adithyas-projects-e20db2dd.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://task-management-frontend-313q77smc-adithyas-projects-e20db2dd.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
+app.options("*", cors());
 app.use(express.json());
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
